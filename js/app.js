07b07b4cde97faa5463b705beb4c2758469ca229
zorazky <oswaldo.vazquez@gmail.com>
2019-12-02 23:44:18 -0600

    adding animation to the case page

--- js/app.js
+++ js/app.js
@@ -1,47 +1,51 @@
-var tl = gsap.timeline({onUpdate:updatePercentage})
+var tlstart = gsap.timeline();
+var tl = gsap.timeline();
 var tl2 = gsap.timeline();
 var tl3 = gsap.timeline();
 
 const controller = new ScrollMagic.Controller();
 
-tl2.from("#background", 1, {opacity:0, y: "-=20"}, "+=.3")
+tlstart.from("#background", 1, {opacity:0, y: "-=20"}, "+=.3")
     .from(".anim1", .8, {opacity:0, x:-200, stagger: .4})
     .from(".sectionProjects", .2, {opacity:0, y:10}, "-=1");
-    
-tl3.from(".anim4",.6, {opacity:0, y: "-=10",stagger: .3})
 
 
 tl.from(".anim2", .6, {opacity: 0, x:"-=200", stagger:.2})
-    .from(".projectThumb1", .4, {opacity:0, x:"+=20"}, "-=.6")
-    .from(".anim3", .6, {opacity: 0, x:"+=50", stagger:.2})
-    .from(".projectThumb2", .4, {opacity:0, x:"-=20"}, "-=.8")
+    .from(".projectThumb1", .4, {opacity:0, x:"+=20"}, "-=.8");
+    
+
+tl2.from(".anim3", .6, {opacity: 0, x:"+=50", stagger:.2})
+    .from(".projectThumb2", .4, {opacity:0, x:"-=20"}, "-=.9");
+
+
+tl3.from(".anim4",.6, {opacity:0, y: "-=10", stagger: .3});
     
 
 
 
 const scene = new ScrollMagic.Scene({
-    triggerElement: "#trigger1",
-    duration: "200%"
+    triggerElement: "#trigger1"
 })
 
-.setPin("#trigger1")
-    .setTween(tl)
-        .addTo(controller);
+
+.setTween(tl)
+    .addTo(controller);
 
 const scene2 = new ScrollMagic.Scene({
-    triggerElement: "#trusted"
+    triggerElement: "#trigger2"
+})
+
+.setTween(tl2)
+    .addTo(controller);
+
+const scene3 = new ScrollMagic.Scene({
+    triggerElement: "#trigger3"
 })
 
 .setTween(tl3)
-.addTo(controller);
+    .addTo(controller);
 
 
         
 
 
-function updatePercentage() {
-    //percent.innerHTML = (tl.progress() *100 ).toFixed();
-    tl.progress();
-    
-    //console.log(tl.progress());
-    }
---
+++ js/tripwing.js
@@ -0,0 +1,54 @@
+var tlstart = gsap.timeline();
+var tl = gsap.timeline();
+var tl2 = gsap.timeline();
+var tl3 = gsap.timeline();
+var tl4 = gsap.timeline();
+
+const controller = new ScrollMagic.Controller();
+
+tlstart.from(".logo_center", .8, {opacity:0, y: "-=20"}, "+=.3")
+    .from(".case_content", .6, {opacity:0, y:"+=10"}, "+=.3")
+    .from(".info", .4, {opacity:0, y:"-=10"});
+
+tl.from(".anim1", .6, {opacity: 0, x:"-=50", stagger:.3});
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: ".trigger1"
+})
+
+
+.setTween(tl)
+    .addTo(controller);
+
+tl2.from(".flow1", .6, {opacity: 0, x:"-=50"})
+    .from(".flow2", .6, {opacity: 0, x:"+=50"})
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: ".trigger2"
+})
+
+
+.setTween(tl2)
+    .addTo(controller);
+
+tl3.from(".ui1", .6, {opacity: 0, y:"-=50"})
+.from(".ui2", .6, {opacity: 0, y:"-=50"})
+
+const scene3 = new ScrollMagic.Scene({
+    triggerElement: ".trigger3"
+})
+
+
+.setTween(tl3)
+    .addTo(controller);
+
+tl4.from(".mock1", .6, {opacity: 0, y:"+=50"})
+.from(".mock2", .6, {opacity: 0, y:"+=50"})
+
+const scene4 = new ScrollMagic.Scene({
+    triggerElement: ".trigger4"
+})
+
+
+.setTween(tl4)
+    .addTo(controller);
