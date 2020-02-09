d9457d605cfa765df900f5846931ffc7f2ce9e4b
zorazky <oswaldo.vazquez@gmail.com>
2020-02-09 13:40:54 -0600

    animation is broken but not sure why

--- js/caseAtlas.js
+++ js/caseAtlas.js
@@ -1,5 +1,11 @@
 
+var tlstart = gsap.timeline();
+var tl = gsap.timeline();
+var tl2 = gsap.timeline();
+var tl3 = gsap.timeline();
+var tl4 = gsap.timeline();
 
+const controller = new ScrollMagic.Controller();
 
 
 let counter = document.querySelector('.counter');
@@ -10,9 +16,10 @@ let count = 0;
 
 var queue = new createjs.LoadQueue(false);
 
+body.classList.add("blockScroll")
 queue.on("progress", event => {
     console.log(event);
-    body.classList.add("blockScroll")
+    
     let progress = Math.floor(event.progress * 100);
 
     counter.textContent = `${progress}%`;
@@ -64,6 +71,61 @@ queue.loadFile('../atlas/img/prototype2.mp4');
 queue.loadFile('../atlas/img/prototype3.mp4');
 queue.loadFile('../atlas/img/prototype4.mp4');
 queue.loadFile('../atlas/img/ui_mock_1.png');
+
+
+
+
+
+tlstart.from(".logo_center", .6, {opacity:0, y: "-=20"}, "+=.2")
+.from(".case_content", .5, {opacity:0, y:"+=10"}, "+=.2")
+.from(".info", .4, {opacity:0, y:"-=10"})
+
+
+
+
+
+tl.from(".anim1", .4, {opacity: 0, x:"-=50", stagger:.2});
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: ".trigger1"
+})
+
+
+.setTween(tl)
+    .addTo(controller);
+
+tl2.from(".flow1", .4, {opacity: 0, x:"-=50"})
+    .from(".flow2", .4, {opacity: 0, x:"-=50"})
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: ".trigger2"
+})
+
+
+.setTween(tl2)
+    .addTo(controller);
+
+tl3.from(".ui1", .4, {opacity: 0, y:"-=50"})
+.from(".ui2", .4, {opacity: 0, y:"-=50"})
+
+const scene3 = new ScrollMagic.Scene({
+    triggerElement: ".trigger3"
+})
+
+
+.setTween(tl3)
+    .addTo(controller);
+
+tl4.from(".mock1", .4, {opacity: 0, y:"+=50"})
+.from(".mock2", .4, {opacity: 0, y:"+=50"})
+
+const scene4 = new ScrollMagic.Scene({
+    triggerElement: ".trigger4"
+})
+
+
+.setTween(tl4)
+    .addTo(controller);
 
 
 
